2b8ba1e5384bc36813f57eef11ab57c315a74f97
Andy Fragen <andy@thefragens.com>
2019-07-25 12:15:19 -0700

    cleanup comments

--- install-mas.sh
+++ install-mas.sh
@@ -26,25 +26,26 @@ mas install \
     775737590 \
     409183694
 
-#    1278508951 \ #Trello
-#    1333542190 \ # 1Password 7
-#    1378283354 \ # Whalebird
-#    639210716 \ # NeoOffice
-#    514489722 \ # VoodooPad
-##    682658836 \ # GarageBand
-#    429449079 \ # Patterns
-#    497799835 \ # Xcode
-#    430397512 \ # SnapNDrag Pro
-#    1384080005 \ # Tweetbot
-#    1037126344 \ # Apple Configurator 2
-#    1289583905 \ # Pixelmator Pro
-#    425424353 \ # The Unarchiver
-#    621276561 \ # PDFOptim
-#    1179623856 \ # Pastebot
-#    409201541 \ # Pages
-#    418778021 \ # MsgFiler
-#    1437227804 \ # XDebugToggle
-#    408981434 \ # iMovie
-#    409203825 \ # Numbers
-#    775737590 \ # iA Writer
+#    1278508951 #Trello
+#    1333542190 # 1Password 7
+#    1378283354 # Whalebird
+#    639210716 # NeoOffice
+#    514489722 # VoodooPad
+#    429449079 # Patterns
+#    497799835 # Xcode
+#    430397512 # SnapNDrag Pro
+#    1384080005 # Tweetbot
+#    1037126344 # Apple Configurator 2
+#    937984704 # Amphetamine
+#    1289583905 # Pixelmator Pro
+#    425424353 # The Unarchiver
+#    621276561 # PDFOptim
+#    1179623856 # Pastebot
+#    409201541 # Pages
+#    418778021 # MsgFiler
+#    1437227804 # XDebugToggle
+#    408981434 # iMovie
+#    409203825 # Numbers
+#    775737590 # iA Writer
 #    409183694 # Keynote
+#    682658836 # GarageBand
--- install-quicklook.sh
+++ install-quicklook.sh
@@ -12,12 +12,12 @@ brew cask install \
     qlimagesize \
     suspicious-package
 
-#    qlcolorcode \ # Preview source code files with syntax highlighting
-#    qlstephen \ # Preview plain text files without a file extension. Example: README, #CHANGELOG, etc.
-#    qlmarkdown \ # Preview Markdown files
-#    quicklook-json \ # Preview JSON files
-#    qlprettypatch \ # Preview .patch files
-#    quicklook-csv \ # Preview CSV files
-#    betterzip \ # Preview archives
-#    qlimagesize \ # Display image size and resolution
+#    qlcolorcode # Preview source code files with syntax highlighting
+#    qlstephen # Preview plain text files without a file extension. Example: README, CHANGELOG, etc.
+#    qlmarkdown # Preview Markdown files
+#    quicklook-json # Preview JSON files
+#    qlprettypatch # Preview .patch files
+#    quicklook-csv # Preview CSV files
+#    betterzip # Preview archives
+#    qlimagesize # Display image size and resolution
 #    suspicious-package # Preview the contents of a standard Apple installer package
