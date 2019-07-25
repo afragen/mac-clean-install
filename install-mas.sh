2ee52df2d274bb38eb37e7eff675eba1e55a3ff5
Andy Fragen <andy@thefragens.com>
2019-07-25 11:06:20 -0700

    can't put comments inline

--- install-mas.sh
+++ install-mas.sh
@@ -3,25 +3,47 @@
 # Install Mac App Store apps.
 # Get list of installed Mac App Store apps using `mas list`
 mas install \
-    1278508951 \ #Trello
-    1333542190 \ # 1Password 7
-    1378283354 \ # Whalebird
-    639210716 \ # NeoOffice
-    514489722 \ # VoodooPad
-#    682658836 \ # GarageBand
-    429449079 \ # Patterns
-    497799835 \ # Xcode
-    430397512 \ # SnapNDrag Pro
-    1384080005 \ # Tweetbot
-    1037126344 \ # Apple Configurator 2
-    1289583905 \ # Pixelmator Pro
-    425424353 \ # The Unarchiver
-    621276561 \ # PDFOptim
-    1179623856 \ # Pastebot
-    409201541 \ # Pages
-    418778021 \ # MsgFiler
-    1437227804 \ # XDebugToggle
-    408981434 \ # iMovie
-    409203825 \ # Numbers
-    775737590 \ # iA Writer
-    409183694 # Keynote
+    1278508951 \
+    1333542190 \
+    1378283354 \
+    639210716 \
+    514489722 \
+    429449079 \
+    497799835 \
+    430397512 \
+    1384080005 \
+    1037126344 \
+    1289583905 \
+    425424353 \
+    621276561 \
+    1179623856 \
+    409201541 \
+    418778021 \
+    1437227804 \
+    408981434 \
+    409203825 \
+    775737590 \
+    409183694
+
+#    1278508951 \ #Trello
+#    1333542190 \ # 1Password 7
+#    1378283354 \ # Whalebird
+#    639210716 \ # NeoOffice
+#    514489722 \ # VoodooPad
+##    682658836 \ # GarageBand
+#    429449079 \ # Patterns
+#    497799835 \ # Xcode
+#    430397512 \ # SnapNDrag Pro
+#    1384080005 \ # Tweetbot
+#    1037126344 \ # Apple Configurator 2
+#    1289583905 \ # Pixelmator Pro
+#    425424353 \ # The Unarchiver
+#    621276561 \ # PDFOptim
+#    1179623856 \ # Pastebot
+#    409201541 \ # Pages
+#    418778021 \ # MsgFiler
+#    1437227804 \ # XDebugToggle
+#    408981434 \ # iMovie
+#    409203825 \ # Numbers
+#    775737590 \ # iA Writer
+#    409183694 # Keynote
--- install-quicklook.sh
+++ install-quicklook.sh
@@ -2,12 +2,22 @@
 
 # Install QuickLook plugins.
 brew cask install \
-    qlcolorcode \ # Preview source code files with syntax highlighting
-    qlstephen \ # Preview plain text files without a file extension. Example: README, CHANGELOG, etc.
-    qlmarkdown \ # Preview Markdown files
-    quicklook-json \ # Preview JSON files
-    qlprettypatch \ # Preview .patch files
-    quicklook-csv \ # Preview CSV files
-    betterzip \ # Preview archives
-    qlimagesize \ # Display image size and resolution
-    suspicious-package # Preview the contents of a standard Apple installer package
+    qlcolorcode \
+    qlstephen \
+    qlmarkdown \
+    quicklook-json \
+    qlprettypatch \
+    quicklook-csv \
+    betterzip \
+    qlimagesize \
+    suspicious-package
+
+#    qlcolorcode \ # Preview source code files with syntax highlighting
+#    qlstephen \ # Preview plain text files without a file extension. Example: README, #CHANGELOG, etc.
+#    qlmarkdown \ # Preview Markdown files
+#    quicklook-json \ # Preview JSON files
+#    qlprettypatch \ # Preview .patch files
+#    quicklook-csv \ # Preview CSV files
+#    betterzip \ # Preview archives
+#    qlimagesize \ # Display image size and resolution
+#    suspicious-package # Preview the contents of a standard Apple installer package
