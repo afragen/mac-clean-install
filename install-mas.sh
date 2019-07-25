7da917f3e1a1e3292c8e37f7c3ae35cb34136579
Andy Fragen <andy@thefragens.com>
2019-07-25 10:57:20 -0700

    remove version info

--- install-mas.sh
+++ install-mas.sh
@@ -3,25 +3,25 @@
 # Install Mac App Store apps.
 # Get list of installed Mac App Store apps using `mas list`
 mas install \
-    1278508951 \ #Trello (2.10.14)
-    1333542190 \ # 1Password 7 (7.3.1)
-    1378283354 \ # Whalebird (2.8.1)
-    639210716 \ # NeoOffice (2017.18)
-    514489722 \ # VoodooPad (5.3.0)
-#    682658836 \ # GarageBand (10.3.2)
-    429449079 \ # Patterns (1.2)
-    497799835 \ # Xcode (10.3)
-    430397512 \ # SnapNDrag Pro (4.3.1)
-    1384080005 \ # Tweetbot (3.3)
-    1037126344 \ # Apple Configurator 2 (2.9)
-    1289583905 \ # Pixelmator Pro (1.4)
-    425424353 \ # The Unarchiver (4.1.0)
-    621276561 \ # PDFOptim (2.3.1)
-    1179623856 \ # Pastebot (2.2.1)
-    409201541 \ # Pages (8.1)
-    418778021 \ # MsgFiler (3.1.4)
-    1437227804 \ # XDebugToggle (1.2)
-    408981434 \ # iMovie (10.1.12)
-    409203825 \ # Numbers (6.1)
-    775737590 \ # iA Writer (5.2.7)
-    409183694 # Keynote (9.1)
+    1278508951 \ #Trello
+    1333542190 \ # 1Password 7
+    1378283354 \ # Whalebird
+    639210716 \ # NeoOffice
+    514489722 \ # VoodooPad
+#    682658836 \ # GarageBand
+    429449079 \ # Patterns
+    497799835 \ # Xcode
+    430397512 \ # SnapNDrag Pro
+    1384080005 \ # Tweetbot
+    1037126344 \ # Apple Configurator 2
+    1289583905 \ # Pixelmator Pro
+    425424353 \ # The Unarchiver
+    621276561 \ # PDFOptim
+    1179623856 \ # Pastebot
+    409201541 \ # Pages
+    418778021 \ # MsgFiler
+    1437227804 \ # XDebugToggle
+    408981434 \ # iMovie
+    409203825 \ # Numbers
+    775737590 \ # iA Writer
+    409183694 # Keynote
