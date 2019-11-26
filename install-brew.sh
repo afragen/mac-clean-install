970c65a9abaf6a6bea9e2512c71480d83377b8f8
Andy Fragen <andy@thefragens.com>
2019-11-25 22:13:59 -0800

    specify Brewfile

--- install-brew.sh
+++ install-brew.sh
@@ -3,6 +3,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update && brew upgrade && brew cleanup && brew doctor
 
-brew bundle install
+brew bundle install --file=~/Brewfile
 
 #cask "https://www.corecode.io/macupdater/casks/citrix-workspace.rb"
