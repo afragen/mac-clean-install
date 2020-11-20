1076e8cc761a1ea7432da86a94f0d26538abb128
Andy Fragen <andy@thefragens.com>
2020-11-20 10:48:47 -0800

    new homebrew installation

--- install-brew.sh
+++ install-brew.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Install Homebrew.
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update && brew upgrade && brew cleanup && brew doctor
 
 brew bundle install --file=~/Brewfile
